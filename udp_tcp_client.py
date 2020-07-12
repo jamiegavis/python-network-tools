c083c63c883f80d54a4336f46bdee6015c388bd7
EJG <gavis.edwin@gmail.com>
2020-07-11 20:37:35 -0500

    updated paramaters for utility compatibility

--- udp_tcp_client.py
+++ udp_tcp_client.py
@@ -9,33 +9,44 @@ Created on Fri Jul  3 08:46:38 2020
 import socket
 import sys
 
-def main(host,port,data,sock_type=socket.SOCK_STREAM):
+def connect(host,port,data,sock_type=socket.SOCK_STREAM):
     '''
     Sends packets (TCP/UDP) and returns response from server.
     
-    Does not handle bad connection attempts, slow response times or 
-    servers that send data first and expect response.
+    Does not handle bad connection attempts or slow response times.
     
     Inputs
         sock_type: Type of socket. (defaults to socket.SOCK_STREAM)
         host: the host server (127.0.0.1, www.cnn.com etc)
         port: target port
-        data: data to be sent        
+        data: data to be sent (if None: TCP will connect but not send data)        
             
     Returns
-        data from server
+        data from server or 0 if TCP error
     
     '''
     client = socket.socket(socket.AF_INET,sock_type) #default paramaters for tcp
-    
     if sock_type == socket.SOCK_STREAM:
-        client.connect((host,port))
-        client.send(data)
-        return client.recv(4096)
+        try:
+            client.connect((host,port))
+            if data:
+                client.send(data)
+            resp_len,response = 1,""
+            while resp_len:
+                client_data = client.recv(4096)
+                resp_len = len(client_data)
+                response += client_data
+                if resp_len < 4096:
+                    break    
+            return response
+        except Exception as e:
+            print("Exception: ",e ,"-- Exiting.")
+            client.close()
+            return 0
     
     elif sock_type == socket.SOCK_DGRAM:
         client.sendto(data,(host,port))
-        received,_ = client.recvfrom(4096)
+        received,_ = client.recvfrom(4096) #resp size limited to 4096
         return received
     
     
@@ -52,4 +63,4 @@ if __name__ == "__main__":
     elif sys.argv[1] == "-u":
         sock = socket.SOCK_DGRAM
     
-    main(host,port,data,sock_type=socket.SOCK_STREAM)
+    connect(host,port,data,sock_type=socket.SOCK_STREAM)
